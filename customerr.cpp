a4fabf64829d6824c999eecd8b9c3eac3059db12
hareemasif26 <hareemasif2609@gmail.com>
2026-03-28 07:35:24 -0700

    week 6 work

---
+++ customerr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, count = 0;
+    cout<<"Enter the names of customers:";
+    cin >> n;
+    string customers[n];
+    cout<<"Enter the names of"<< n <<"customers:" << endl;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> customers[i];
+    }
+    char letter;
+    cout<<"Enter a letter to check:";
+    cin>>letter;
+    for(int i = 0; i < n; i++)
+    {
+        if(customers[i][0] == letter)
+        {
+            count++;
+        }
+    }
+    cout<<"Total names starting with"<<letter<<" : "<<count<<endl;
+}
