948b00628531b81e5ffead648a655d7e508f15ed
hareemasif26 <hareemasif2609@gmail.com>
2026-03-28 07:08:28 -0700

    lab 6 work

---
+++ largestt.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int findLargestnumber(int arr[], int size)
+{
+    int largest = arr[0];
+    for(int i = 1; i < size; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter"<<n<<"numbers,one per line:"<<endl;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int largest = findLargestnumber(arr,n);
+    cout<<"The largest number entered is:"<<largest;
+}
